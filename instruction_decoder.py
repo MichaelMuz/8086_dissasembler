78876b44410c61ca50df8bd5200758812ff87b73
Michael Muz <20muzm01@gmail.com>
2025-04-30 20:33:19 -0400

    memory moves too

--- instruction_decoder.py
+++ instruction_decoder.py
@@ -5,23 +5,39 @@ import os
 
 
 def parse_file_and_get_dissasembled_instructions(inp_file_name):
-    instructions = []
     with open(inp_file_name, "rb") as file:
-        binary = file.read()
-        for byte1, byte2 in itertools.batched(binary, 2, strict=True):
-            # bit 7 indicated direction of the move, 0 means source is reg field, 1 means destination in reg field
-            direction_set = bool(byte1 & 0b00000010)
+        file_contents: bytes = file.read()
 
+    binary = iter(file_contents)
+    latest_byte = None
+    instructions = []
+    while latest_byte is not StopIteration:
+        byte1 = next(binary)
+        if (byte1 & 0b10001000) == 0b10001000:
+            # bit 7 indicated direction of the move, 0 means source is reg field, 1 means destination in reg field
+            direction_bit = bool(byte1 & 0b00000010)
             # bit 8 tells us if we use the 16 bit register or just lower 8 bits
-            word_16_bit_set = bool(byte1 & 0b00000001)
-            if (byte1 & 0b10001000) == 0b10001000:
-                # in byte 2 first 2 bits are mode bits tell us what kind of move
-                # 11 tells us register to register
-                mode_bits = byte2 & 0b11000000
-                assert (
-                    mode_bits == 0b11000000
-                ), "Only deal with mov register to register, memory mov not expected"
-                # indexed by register number code, then indexed by if we are using the full word
+            word_bit_set = bool(byte1 & 0b00000001)
+
+            byte2 = next(binary)
+            mode_bits = byte2 & 0b11000000 >> 6
+
+            r_m_bits = byte2 & 0b00000111
+
+            r_m_to_effective_addr_calc = [
+                # if there are two things in the list, the equation these bits code for are those added
+                ["bx", "si"],
+                ["bx", "di"],
+                ["bp", "si"],
+                ["bp", "di"],
+                ["si"],
+                ["di"],
+                ["bp"],
+                ["bx"],
+            ]
+
+            # if mode is 11 it is register to register and we have a second register as the second operand
+            if mode_bits == 0b11:
                 reg_name_lower_and_word = [
                     # [lower_register_name, word_full_register_name]
                     ["al", "ax"],
@@ -35,27 +51,43 @@ def parse_file_and_get_dissasembled_instructions(inp_file_name):
                 ]
                 # first register is these bits
                 reg_field_val = (byte2 & 0b00111000) >> 3
-                reg_field_operand = reg_name_lower_and_word[reg_field_val][
-                    word_16_bit_set
-                ]
+                reg_field_operand = reg_name_lower_and_word[reg_field_val][word_bit_set]
 
                 # if mode is 11 it is register to register and we have a second register as the second operand
-                r_m_field_val = byte2 & 0b00000111
-                r_m_field_operand = reg_name_lower_and_word[r_m_field_val][
-                    word_16_bit_set
-                ]
+                r_m_field_operand = reg_name_lower_and_word[r_m_bits][word_bit_set]
 
                 source_dest = [reg_field_operand, r_m_field_operand]
-                if direction_set:
+                if direction_bit:
                     source_dest = list(reversed(source_dest))
                 source, dest = source_dest
 
                 instructions.append(f"mov {dest}, {source}")
-            else:
 
-                raise ValueError(
-                    f"Didn't expect to get here, given input was: {" ".join([f"{b:08b}" for b in binary])}"
+            else:
+                equation = r_m_to_effective_addr_calc[r_m_bits]
+
+                displacement = 0
+                # 8 bit displacement
+                if mode_bits & 0b01:
+                    low_disp_byte = next(binary)
+                    displacement += low_disp_byte
+
+                # when mode is 0 no displacement unless r/m field is 11
+                # when mode is 10 then 16 bit displacement
+                if mode_bits & 0b10 or (mode_bits == 0 and r_m_bits == 0b00):
+                    high_disp_byte = next(binary)
+                    displacement += high_disp_byte << 8
+
+                instructions.append(
+                    " + ".join(equation)
+                    + (f" + {displacement}" if displacement > 0 else "")
                 )
+
+        else:
+
+            raise ValueError(
+                f"Didn't expect to get here, given input was: {" ".join([f"{b:08b}" for b in binary])}"
+            )
     return instructions
 
 
