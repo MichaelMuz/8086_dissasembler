b3628d63208b174e510ead3138900fc7ee76d54e
Michael Muz <20muzm01@gmail.com>
2025-09-07 22:03:07 -0400

    more testcases

--- python_implementation/test/test_disassembler.py
+++ python_implementation/test/test_disassembler.py
@@ -178,3 +178,175 @@ class TestMov(TestDisassembler):
 
     def test_accumulator_to_memories(self):
         self.help_test_given_asm(["mov [2554], ax", "mov [15], ax"])
+
+
+class TestSub(TestDisassembler):
+    def test_sub_reg_from_memory(self):
+        self.help_test_given_asm(["sub bx, [bx+si]", "sub bx, [bp]"])
+
+    def test_sub_immediate_from_reg(self):
+        self.help_test_given_asm(["sub si, 2", "sub bp, 2", "sub cx, 8"])
+
+    def test_sub_reg_from_memory_with_displacement(self):
+        self.help_test_given_asm(
+            [
+                "sub bx, [bp + 0]",
+                "sub cx, [bx + 2]",
+                "sub bh, [bp + si + 4]",
+                "sub di, [bp + di + 6]",
+            ]
+        )
+
+    def test_sub_reg_from_memory_dest(self):
+        self.help_test_given_asm(
+            [
+                "sub [bx+si], bx",
+                "sub [bp], bx",
+                "sub [bp + 0], bx",
+                "sub [bx + 2], cx",
+                "sub [bp + si + 4], bh",
+                "sub [bp + di + 6], di",
+            ]
+        )
+
+    def test_sub_immediate_from_memory(self):
+        self.help_test_given_asm(["sub byte [bx], 34", "sub word [bx + di], 29"])
+
+    def test_sub_mixed_operations(self):
+        self.help_test_given_asm(
+            ["sub ax, [bp]", "sub al, [bx + si]", "sub ax, bx", "sub al, ah"]
+        )
+
+    def test_sub_immediate_values(self):
+        self.help_test_given_asm(["sub ax, 1000", "sub al, -30", "sub al, 9"])
+
+
+# class TestAdd(TestDisassembler):
+#     def test_add_reg_from_memory(self):
+#         self.help_test_given_asm(["add bx, [bx+si]", "add bx, [bp]"])
+
+#     def test_add_immediate_to_reg(self):
+#         self.help_test_given_asm(["add si, 2", "add bp, 2", "add cx, 8"])
+
+#     def test_add_reg_from_memory_with_displacement(self):
+#         self.help_test_given_asm(
+#             [
+#                 "add bx, [bp + 0]",
+#                 "add cx, [bx + 2]",
+#                 "add bh, [bp + si + 4]",
+#                 "add di, [bp + di + 6]",
+#             ]
+#         )
+
+#     def test_add_reg_to_memory(self):
+#         self.help_test_given_asm(
+#             [
+#                 "add [bx+si], bx",
+#                 "add [bp], bx",
+#                 "add [bp + 0], bx",
+#                 "add [bx + 2], cx",
+#                 "add [bp + si + 4], bh",
+#                 "add [bp + di + 6], di",
+#             ]
+#         )
+
+#     def test_add_immediate_to_memory(self):
+#         self.help_test_given_asm(["add byte [bx], 34", "add word [bp + si + 1000], 29"])
+
+#     def test_add_mixed_operations(self):
+#         self.help_test_given_asm(
+#             ["add ax, [bp]", "add al, [bx + si]", "add ax, bx", "add al, ah"]
+#         )
+
+#     def test_add_immediate_values(self):
+#         self.help_test_given_asm(["add ax, 1000", "add al, -30", "add al, 9"])
+
+
+# class TestCmp(TestDisassembler):
+#     def test_cmp_reg_with_memory(self):
+#         self.help_test_given_asm(["cmp bx, [bx+si]", "cmp bx, [bp]"])
+
+#     def test_cmp_reg_with_immediate(self):
+#         self.help_test_given_asm(["cmp si, 2", "cmp bp, 2", "cmp cx, 8"])
+
+#     def test_cmp_reg_with_memory_displacement(self):
+#         self.help_test_given_asm(
+#             [
+#                 "cmp bx, [bp + 0]",
+#                 "cmp cx, [bx + 2]",
+#                 "cmp bh, [bp + si + 4]",
+#                 "cmp di, [bp + di + 6]",
+#             ]
+#         )
+
+#     def test_cmp_memory_with_reg(self):
+#         self.help_test_given_asm(
+#             [
+#                 "cmp [bx+si], bx",
+#                 "cmp [bp], bx",
+#                 "cmp [bp + 0], bx",
+#                 "cmp [bx + 2], cx",
+#                 "cmp [bp + si + 4], bh",
+#                 "cmp [bp + di + 6], di",
+#             ]
+#         )
+
+#     def test_cmp_memory_with_immediate(self):
+#         self.help_test_given_asm(["cmp byte [bx], 34", "cmp word [4834], 29"])
+
+#     def test_cmp_mixed_operations(self):
+#         self.help_test_given_asm(
+#             ["cmp ax, [bp]", "cmp al, [bx + si]", "cmp ax, bx", "cmp al, ah"]
+#         )
+
+#     def test_cmp_immediate_values(self):
+#         self.help_test_given_asm(["cmp ax, 1000", "cmp al, -30", "cmp al, 9"])
+
+
+# class TestJumps(TestDisassembler):
+#     def test_jnz_instructions(self):
+#         self.help_test_given_asm(
+#             [
+#                 "test_label0:",
+#                 "jnz test_label1",
+#                 "jnz test_label0",
+#                 "test_label1:",
+#                 "jnz test_label0",
+#                 "jnz test_label1",
+#             ]
+#         )
+
+#     def test_conditional_jumps(self):
+#         self.help_test_given_asm(
+#             [
+#                 "label:",
+#                 "je label",
+#                 "jl label",
+#                 "jle label",
+#                 "jb label",
+#                 "jbe label",
+#                 "jp label",
+#                 "jo label",
+#                 "js label",
+#             ]
+#         )
+
+#     def test_negative_conditional_jumps(self):
+#         self.help_test_given_asm(
+#             [
+#                 "label:",
+#                 "jne label",
+#                 "jnl label",
+#                 "jg label",
+#                 "jnb label",
+#                 "ja label",
+#                 "jnp label",
+#                 "jno label",
+#                 "jns label",
+#             ]
+#         )
+
+#     def test_loop_instructions(self):
+#         self.help_test_given_asm(
+#             ["label:", "loop label", "loopz label", "loopnz label", "jcxz label"]
+#         )
