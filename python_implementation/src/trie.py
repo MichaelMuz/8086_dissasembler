e8d1e26ca485147c5e19eaf37b1b1d38eb596879
Michael Muz <20muzm01@gmail.com>
2025-09-14 22:30:07 -0400

    more

--- python_implementation/src/trie.py
+++ python_implementation/src/trie.py
@@ -1,6 +1,7 @@
 from typing import Iterator, Self, TypeAlias
 from dataclasses import dataclass
 import itertools
+from python_implementation.src.builder import DisassembledInstruction
 from python_implementation.src.schema import (
     InstructionSchema,
     LiteralField,
@@ -8,6 +9,7 @@ from python_implementation.src.schema import (
     SchemaField,
 )
 from python_implementation.src.utils import get_sub_most_sig_bits
+from python_implementation.src.decoder import BitIterator
 
 
 @dataclass
@@ -74,6 +76,17 @@ def insert_into_trie(
 
 
 class Trie:
+    ALWAYS_NEEDED_FIELDS = {
+        # if we see this in an instruction schema, we must always parse it
+        NamedField.D,
+        NamedField.W,
+        NamedField.S,
+        NamedField.MOD,
+        NamedField.REG,
+        NamedField.RM,
+        NamedField.DATA,
+    }
+
     def __init__(self, head: BitNode) -> None:
         self.head = head
 
@@ -84,3 +97,38 @@ class Trie:
             head = insert_into_trie(head, expand_fields_to_bits(instruction.fields))
         assert isinstance(head, BitNode), f"Expected BitNode, got `{type(head)}`"
         return cls(head)
+
+    def _is_needed(self, schema_field: SchemaField, parsed_fields) -> bool:
+        if isinstance(schema_field, LiteralField):
+            return True
+
+        if schema_field in self.ALWAYS_NEEDED_FIELDS:
+            return True
+        elif schema_field is NamedField.DATA_IF_W1:
+            return bool(self.parsed_fields[NamedField.W])
+        elif schema_field is NamedField.DATA_IF_SW_01:
+            return self.sign_extension == 0 and self.word
+        elif schema_field in (NamedField.DISP_LO, NamedField.DISP_HI):
+            return (self.mode.type is Mode.Type.WORD_DISPLACEMENT_MODE) or (
+                self.mode.type is Mode.Type.BYTE_DISPLACEMENT_MODE
+                and (schema_field is NamedField.DISP_LO)
+            )
+        else:
+            raise ValueError(f"don't know how to check if {schema_field} is needed")
+
+    def _parse_rec(
+        self, head: Node | None, bit_iter: BitIterator
+    ) -> DisassembledInstruction:
+        if head is None:
+            return
+        elif isinstance(head, BitNode):
+            if bit_iter.next_bits(1):
+                return self._parse_rec(head.right, bit_iter)
+            else:
+                return self._parse_rec(head.left, bit_iter)
+        else:
+
+            field_value = bit_iter.next_bits(head.named_field.bit_width)
+
+    def parse(self, bit_iter: BitIterator) -> DisassembledInstruction:
+        self._parse_rec(self, self.head, bit_iter)
