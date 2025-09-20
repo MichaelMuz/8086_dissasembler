2bde3036570201b87985b346e07b66eb4c70e3f6
Michael Muz <20muzm01@gmail.com>
2025-09-20 14:43:25 -0400

    prog

--- python_implementation/src/trie.py
+++ python_implementation/src/trie.py
@@ -1,6 +1,7 @@
-from typing import Iterator, Self, TypeAlias
+from typing import Generator, Iterator, Self, TypeAlias
 from dataclasses import dataclass
 import itertools
+from python_implementation.src import utils
 from python_implementation.src.builder import DecodeAccumulator
 from python_implementation.src.schema import (
     InstructionSchema,
@@ -27,7 +28,44 @@ class FieldNode:
 @dataclass
 class LeafNode:
     instruction: InstructionSchema
+    coil_start: NamedField | None
     token_iter: Iterator[NamedField | bool]
+
+
+def bin_iter(field: LiteralField) -> Generator[bool, None, None]:
+    for i in range(field.bit_width):
+        yield bool(utils.get_sub_most_sig_bits(field.literal_value, i, 1))
+
+
+class InstructionIterator:
+    def __init__(self, instruction: InstructionSchema) -> None:
+        self.instruction = instruction
+        self.ind = 0
+        self.sub_iter = None
+
+    def get_bit_or_named_field(self) -> bool | NamedField:
+        next_up = None
+        if self.sub_iter is not None:
+            next_up = next(self.sub_iter, None)
+        if next_up is None:
+            self.sub_iter = None
+            match self.instruction.fields[self.ind]:
+                case LiteralField() as x:
+                    self.sub_iter = bin_iter(x)
+                    next_up = next(self.sub_iter)
+                case NamedField() as x:
+                    next_up = x
+            self.ind += 1
+
+        assert next_up is not None, "Somehow next is None"
+        return next_up
+
+    def get_whole_field(self):
+        if self.sub_iter is not None:
+            unfinished_lit = ((1 << i * int(e)) for i, e in self.sub_iter)
+            return unfinished_lit
+        else:
+            return
 
 
 Node: TypeAlias = BitNode | FieldNode | LeafNode
@@ -55,11 +93,16 @@ def insert_into_trie(
     current_token = next(token_iter, None)
     if current_token is None:
         assert head is None, "Instruction ends while another continues, ambiguous"
-        head = LeafNode(instruction, token_iter)
+        head = LeafNode(instruction, None, token_iter)
 
     elif head is None:
         # No comparison? We are a coiled branch that will unfold lazily
-        head = LeafNode(instruction, itertools.chain([current_token], token_iter))
+        if isinstance(current_token, bool):
+            # never coil on literal field, we don't want to split fields
+            head = BitNode()
+        else:
+            # can coil on a named field, we are not splitting things
+            head = LeafNode(instruction, current_token, token_iter)
 
     elif isinstance(head, BitNode):
         assert isinstance(
@@ -80,22 +123,17 @@ def insert_into_trie(
         head.next = insert_into_trie(head.next, token_iter, instruction)
     else:
         # unspring coiled branch that head is
-        once_uncoiled_head = next(head.token_iter, None)
         assert (
-            once_uncoiled_head is not None
+            head.coil_start is not None
         ), "Asking to unroll fully unrolled instruction"
-        if isinstance(once_uncoiled_head, NamedField):
-            uncoiled_node = FieldNode(named_field=once_uncoiled_head)
-        else:
-            uncoiled_node = BitNode()
+        uncoiled_node = FieldNode(named_field=head.coil_start)
 
-        head_rewind_iter = itertools.chain([once_uncoiled_head], head.token_iter)
         new_rewind_iter = itertools.chain([current_token], token_iter)
 
         # We are comparing the uncoiled thing against itself so we can reatach the rest of the coil
         # only adds iterations for one series of bits or one named field, then goes back to being coiled
         # only one extra iteration on top of what it does otherwise for bits (this iteration) and for fields it is just 2 because we assert it is correct and the next gives a leaf node
-        head = insert_into_trie(uncoiled_node, head_rewind_iter, head.instruction)
+        head = insert_into_trie(uncoiled_node, head.token_iter, head.instruction)
         # now next instruction will actually add a node to the trie
         head = insert_into_trie(head, new_rewind_iter, instruction)
 
@@ -116,6 +154,26 @@ class Trie:
             )
         assert isinstance(head, BitNode), f"Expected BitNode, got `{type(head)}`"
         return cls(head)
+
+
+# class TrieTraverser:
+#     def __init__(self, trie: Trie) -> None:
+#         self.curr_node = trie.head
+#         self.acc = DecodeAccumulator()
+
+#     def get_req_bits(self) -> int | None:
+#         if isinstance(self.curr_node, BitNode):
+#             return 1
+#         elif isinstance(self.curr_node, FieldNode):
+#             return self.curr_node.named_field.bit_width
+#         else:
+#             return None
+
+#     def progress(self, val: int) -> int:
+#         if isinstance(self.curr_node, BitNode):
+#             return 1
+#         elif isinstance(self.curr_node, FieldNode):
+#             return self.curr_node.named_field.bit_width
 
 
 def parse(trie: Trie, bit_iter: BitIterator):
@@ -143,3 +201,81 @@ def parse(trie: Trie, bit_iter: BitIterator):
                 acc.with_field(e, bit_iter.next_bits(e.bit_width))
 
     return acc.build(head.instruction)
+
+
+# when knowing the full instruction or using the tree that pattern we always have is
+# "ok, how many more bits to read?", the requestors will answer this. One for a Trie and one for normal
+# stream of instruction schemas.
+
+
+class TrieRequester:
+    def __init__(self, trie: Trie, accumulator: DecodeAccumulator):
+        self.current_node = trie.head
+        self.accumulator = accumulator
+        self.instruction_schema = None
+
+    def bits_needed(self) -> int:
+        if isinstance(self.current_node, BitNode):
+            return 1
+        elif isinstance(self.current_node, FieldNode):
+            return self.current_node.named_field.bit_width
+        else:
+            raise ValueError("Should be complete when at LeafNode")
+
+    def consume(self, bits: int) -> None:
+        assert self.current_node is not None, "Current node is None"
+        assert not isinstance(
+            self.current_node, LeafNode
+        ), "Can't consume into Leaf Node"
+
+        if isinstance(self.current_node, BitNode):
+            # self.accumulator.add_bit() in the future or something to preserve actual literal bits too
+            self.current_node = (
+                self.current_node.right if bits else self.current_node.left
+            )
+        elif isinstance(self.current_node, FieldNode):
+            self.accumulator.with_field(self.current_node.named_field, bits)
+            self.current_node = self.current_node.next
+
+    def is_complete(self) -> bool:
+        if isinstance(self.current_node, LeafNode):
+            self.instruction_schema = self.current_node.instruction
+            return True
+        return False
+
+
+class InstructionRequestor:
+    def __init__(
+        self, instruction: InstructionSchema, accumulator: DecodeAccumulator
+    ) -> None:
+        self.instruction = instruction
+        self.accumulator = accumulator
+
+    def bits_needed(self) -> int:
+        if isinstance(self.current_node, BitNode):
+            return 1
+        elif isinstance(self.current_node, FieldNode):
+            return self.current_node.named_field.bit_width
+        else:
+            raise ValueError("Should be complete when at LeafNode")
+
+    def consume(self, bits: int) -> None:
+        assert self.current_node is not None, "Current node is None"
+        assert not isinstance(
+            self.current_node, LeafNode
+        ), "Can't consume into Leaf Node"
+
+        if isinstance(self.current_node, BitNode):
+            # self.accumulator.add_bit() in the future or something to preserve actual literal bits too
+            self.current_node = (
+                self.current_node.right if bits else self.current_node.left
+            )
+        elif isinstance(self.current_node, FieldNode):
+            self.accumulator.with_field(self.current_node.named_field, bits)
+            self.current_node = self.current_node.next
+
+    def is_complete(self) -> bool:
+        if isinstance(self.current_node, LeafNode):
+            self.instruction_schema = self.current_node.instruction
+            return True
+        return False
