5734f36ff4aa3fe25e3c1fabc5925ca5f4ac5ee7
Michael Muz <20muzm01@gmail.com>
2025-09-30 19:03:04 -0400

    more import fixes

--- python_implementation/src/disassembly/instruction.py
+++ python_implementation/src/disassembly/instruction.py
@@ -1,16 +1,10 @@
 from dataclasses import dataclass
-import enum
-from functools import cached_property
-import logging
-from typing import TypeAlias
 
-from python_implementation.src.schema import (
-    InstructionSchema,
-    LiteralField,
-    NamedField,
-    SchemaField,
+from python_implementation.src.parse.operands import (
+    ImmediateOperand,
+    MemoryOperand,
+    Operand,
 )
-from python_implementation.src.utils import as_signed_int, combine_bytes
 
 
 @dataclass(frozen=True)
