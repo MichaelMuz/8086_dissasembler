711977297ab98218312c02866b4b363dc7042347
Michael Muz <20muzm01@gmail.com>
2025-10-04 16:42:55 -0400

    more passing!

--- python_implementation/test/test_trie.py
+++ python_implementation/test/test_trie.py
@@ -86,74 +86,74 @@ class TestBitModeSchemaIterator(unittest.TestCase):
 
 
 class TestTrie(unittest.TestCase):
-    # def test_empty_insert(self):
-    #     head = insert_into_trie(
-    #         None,
-    #         BitModeSchemaIterator(
-    #             InstructionSchema("move", LiteralField(0b101, 3), [NamedField.D], {})
-    #         ),
-    #     )
+    def test_empty_insert(self):
+        head = insert_into_trie(
+            None,
+            BitModeSchemaIterator(
+                InstructionSchema("move", LiteralField(0b101, 3), [NamedField.D], {})
+            ),
+        )
 
-    #     # 1
-    #     assert isinstance(head, BitNode)
-    #     assert head.left is None
+        # 1
+        assert isinstance(head, BitNode)
+        assert head.left is None
 
-    #     # 0
-    #     node = head.right
-    #     assert isinstance(node, BitNode)
-    #     assert node.right is None
+        # 0
+        node = head.right
+        assert isinstance(node, BitNode)
+        assert node.right is None
 
-    #     # bit 1
-    #     node = node.left
-    #     assert isinstance(node, BitNode)
-    #     assert node.left is None
+        # bit 1
+        node = node.left
+        assert isinstance(node, BitNode)
+        assert node.left is None
 
-    #     node = node.right
-    #     assert isinstance(node, LeafNode)
-    #     assert next(node.token_iter) == NamedField.D
+        node = node.right
+        assert isinstance(node, LeafNode)
+        assert next(node.token_iter) == NamedField.D
 
-    #     with self.assertRaises(StopIteration):
-    #         next(node.token_iter)
+        with self.assertRaises(StopIteration):
+            next(node.token_iter)
 
-    # def test_overlapping_path_insert(self):
-    #     head = insert_into_trie(
-    #         None,
-    #         BitModeSchemaIterator(
-    #             InstructionSchema("move", LiteralField(0b101, 3), [NamedField.D], {})
-    #         ),
-    #     )
-    #     head = insert_into_trie(
-    #         head,
-    #         BitModeSchemaIterator(
-    #             InstructionSchema("move", LiteralField(0b100, 3), [NamedField.D], {})
-    #         ),
-    #     )
+    def test_overlapping_path_insert(self):
+        head = insert_into_trie(
+            None,
+            BitModeSchemaIterator(
+                InstructionSchema("move", LiteralField(0b101, 3), [NamedField.D], {})
+            ),
+        )
+        head = insert_into_trie(
+            head,
+            BitModeSchemaIterator(
+                InstructionSchema("move", LiteralField(0b100, 3), [NamedField.D], {})
+            ),
+        )
 
-    #     # 1
-    #     assert isinstance(head, BitNode)
-    #     assert head.left is None
+        # 1
+        assert isinstance(head, BitNode)
+        assert head.left is None
 
-    #     # 0
-    #     node = head.right
-    #     assert isinstance(node, BitNode)
-    #     assert node.right is None
+        # 0
+        node = head.right
+        assert isinstance(node, BitNode)
+        assert node.right is None
 
-    #     # bit 0/1 (diverge)
-    #     node = node.left
-    #     assert isinstance(node, BitNode)
+        # bit 0/1 (diverge)
+        node = node.left
+        assert isinstance(node, BitNode)
 
-    #     one_split = node.right
-    #     two_split = node.left
-    #     assert isinstance(one_split, LeafNode)
-    #     assert isinstance(two_split, LeafNode)
-    #     assert next(one_split.token_iter) == NamedField.D
-    #     assert next(two_split.token_iter) == NamedField.D
+        one_split = node.right
+        two_split = node.left
+        assert isinstance(one_split, LeafNode)
+        assert isinstance(two_split, LeafNode)
+        assert next(one_split.token_iter) == NamedField.D
+        assert next(two_split.token_iter) == NamedField.D
 
-    #     with self.assertRaises(StopIteration):
-    #         next(one_split.token_iter)
+        with self.assertRaises(StopIteration):
+            next(one_split.token_iter)
 
-    #     with self.assertRaises(StopIteration):
-    #         next(two_split.token_iter)
+        with self.assertRaises(StopIteration):
+            next(two_split.token_iter)
 
     def test_overlapping_path_insert(self):
         head = insert_into_trie(
@@ -200,7 +200,7 @@ class TestTrie(unittest.TestCase):
         assert isinstance(node, FieldNode)
         assert node.named_field == NamedField.D
 
-        # 0/1 (diverge again)
+        # 0/1 (diverge on second literal)
         node = node.next
         assert isinstance(node, BitNode)
         old_leaf = node.right
@@ -209,3 +209,77 @@ class TestTrie(unittest.TestCase):
         assert isinstance(old_leaf, LeafNode)
         assert next(old_leaf.token_iter) == NamedField.ADDR_LO
         assert next(new_leaf.token_iter) == NamedField.ADDR_HI
+
+    def test_full_unroll(self):
+        head = insert_into_trie(
+            None,
+            BitModeSchemaIterator(
+                InstructionSchema(
+                    "move",
+                    LiteralField(0b1, 1),
+                    [
+                        NamedField.D,
+                        LiteralField(0b1, 1),
+                        NamedField.ADDR_LO,
+                        LiteralField(0b011, 3),
+                    ],
+                    {},
+                )
+            ),
+        )
+
+        # 1
+        assert isinstance(head, BitNode)
+        assert head.left is None
+
+        node = head.right
+        assert isinstance(node, LeafNode)
+        # curled up on named field
+        assert node.token_iter.is_next_named()
+
+        logger.debug("Inserting another here")
+        head = insert_into_trie(
+            head,
+            BitModeSchemaIterator(
+                InstructionSchema(
+                    "move",
+                    LiteralField(0b1, 1),
+                    [
+                        NamedField.D,
+                        LiteralField(0b1, 1),
+                        NamedField.ADDR_LO,
+                        LiteralField(0b11, 2),
+                    ],
+                    {},
+                )
+            ),
+        )
+        assert isinstance(head, BitNode)
+        assert head.left is None
+        node = head.right
+        assert isinstance(node, FieldNode)
+        assert node.named_field == NamedField.D
+        node = node.next
+        assert isinstance(node, BitNode)
+        assert node.left is None
+        node = node.right
+        assert isinstance(node, FieldNode)
+        assert node.named_field == NamedField.ADDR_LO
+        node = node.next
+        assert isinstance(node, BitNode)
+        assert isinstance(node.left, BitNode) and isinstance(node.right, BitNode)
+
+        l = node.left
+        r = node.right
+        logger.debug(f"RIGHT: {r = }")
+        assert r.left is None and isinstance(r.right, LeafNode)
+        r_leaf = r.right
+        with self.assertRaises(StopIteration):
+            next(r_leaf.token_iter)
+
+        assert l.left is None and isinstance(l.right, BitNode)
+        l = l.right
+        assert l.left is None and isinstance(l.right, LeafNode)
+        l_leaf = l.right
+        with self.assertRaises(StopIteration):
+            next(l_leaf.token_iter)
