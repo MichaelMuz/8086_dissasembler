c7b1021975a70ff997caebef7faeec132ebb543c
20muzm01 <20muzm01@gmail.com>
2026-08-23 14:56:53 -0400

    refactors

--- src/lexer.zig
+++ src/lexer.zig
@@ -53,70 +53,105 @@ const SchemaField = union(enum) {
 
 const MaxFieldsPerInstruction = 8 * 6; // At most 8 one bit things, at most 6 bytes
 
-// TODO I can comptime this so it knows if it is 8 or 16 bits prob?
-const Instruction = struct {
+const InstructionSchema = struct {
     name: []const u8,
     fixed_list: struct {
         fields: [MaxFieldsPerInstruction]SchemaField,
         len: usize,
     },
-
-    // fields: []SchemaField,
     /// the literal bits preserved and 0s for the variable bits
     skeleton: u16,
     /// the literal bits have 1s and the variable bits get 0s
     mask: u16,
 
-    pub fn fields(self: Instruction) []SchemaField {
+    pub fn fields(self: *const InstructionSchema) []SchemaField {
         return self.fixed_list.fields[0..self.fixed_list.len];
     }
 
-    pub fn init(name: []const u8, pattern: []const u8) !Instruction {
-        var skeleton: u16 = 0;
-        var mask: u16 = 0;
+    fn isLiteral(str: []const u8) bool {
+        for (str) |c| {
+            if (c != '1' and c != '0') return false;
+        }
+        return true;
+    }
+
+    fn parseSegment(segment: []const u8) struct { SchemaField, u8, u8 } {
+        const curr_field = undefined;
+        const add_to_skeleton: u8 = 0;
+        const add_to_mask: u8 = 0;
+
+        if (InstructionSchema.isLiteral(segment)) {
+            curr_field = LiteralField(add_to_skeleton);
+            // want to copy these bits to the skeleton and make mask have 1s here
+            // mask gets 1s here
+            add_to_skeleton = std.fmt.parseInt(u8, segment, 2);
+            add_to_mask = (1 << curr_field.width) - 1;
+        } else {
+            curr_field = NamedField.of(segment);
+            // skeleton and mask get 0s here
+        }
+
+        return .{
+            curr_field,
+            add_to_skeleton,
+            add_to_mask,
+        };
+    }
+
+    fn parseByte(byte: []const u8, field_list: *std.ArrayList) struct { u8, u8 } {
+        var skeleton: u8 = 0;
+        var mask: u8 = 0;
         var next_msb: u8 = 0;
 
-        const buffer: [MaxFieldsPerInstruction]SchemaField = undefined;
-        var field_list = std.ArrayList(SchemaField).initBuffer(buffer);
+        var segment_iter = std.mem.tokenizeAny(u8, byte, " ");
+        while (segment_iter.next()) |seg| {
+            const curr_field, const add_to_skeleton, const add_to_mask = InstructionSchema.parseSegment(seg);
+            field_list.append(curr_field);
+            skeleton = utils.insertMostSigBits(u8, skeleton, next_msb, add_to_skeleton, curr_field.width);
+            mask = utils.insertMostSigBits(u8, mask, next_msb, add_to_mask, curr_field.width);
+            next_msb += curr_field.width();
+        }
 
-        for (std.mem.tokenizeAny(u8, pattern, ", ")) |by| {
-            // TODO: refactor this by pulling the inner thing out to a function
-            for (std.mem.tokenizeAny(u8, by, " ")) |seg| {
-                const curr_field = undefined;
-                const add_to_skeleton: u8 = 0;
-                const add_to_mask: u8 = 0;
-                if (std.ascii.isDigit(seg)) {
-                    curr_field = LiteralField(add_to_skeleton);
-                    // want to copy these bits to the skeleton and make mask have 1s here
-                    // mask gets 1s here
-                    add_to_skeleton = std.fmt.parseInt(u8, seg, 2);
-                    add_to_mask = (1 << curr_field.width) - 1;
-                } else {
-                    curr_field = NamedField.of(seg);
-                    // skeleton and mask get 0s here
-                }
-                field_list.append(curr_field);
-                skeleton = utils.insertMostSigBits(u16, skeleton, next_msb, add_to_skeleton, curr_field.width);
-                mask = utils.insertMostSigBits(u16, mask, next_msb, add_to_mask, curr_field.width);
-                next_msb += curr_field.width();
+        std.debug.assert(next_msb == 8);
+
+        return .{ skeleton, mask };
+    }
+
+    pub fn parseInstructionSchema(name: []const u8, pattern: []const u8) !InstructionSchema {
+        var buffer: [MaxFieldsPerInstruction]SchemaField = undefined;
+        const field_list = std.ArrayList(SchemaField).initBuffer(&buffer);
+
+        var skeleton_halfs: [2]u8 = 0;
+        var mask_halfs: [2]u8 = 0;
+        var i = 0;
+
+        var byte_iter = std.mem.tokenizeAny(u8, pattern, ", ");
+        while (byte_iter.next()) |byte| : (i += 1) {
+            const byte_skeleton, const byte_mask = InstructionSchema.parseByte(byte, &field_list);
+            if (i < 2) {
+                skeleton_halfs[i] = byte_skeleton;
+                mask_halfs[i] = byte_mask;
+            } else {
+                std.debug.assert(byte_skeleton == 0);
+                std.debug.assert(byte_mask == 0);
             }
         }
 
         // std.debug.print("skeleton: {s}\n", .{skeleton});
         // std.debug.print("mask: {s}\n", .{mask});
 
-        return Instruction{
+        return InstructionSchema{
             .name = name,
             .fields = .{
                 .fields = buffer,
                 .len = field_list.items.len,
             },
-            .skeleton = skeleton,
-            .mask = mask,
+            .skeleton = std.mem.readInt(u16, skeleton_halfs, .little),
+            .mask = std.mem.readInt(u16, mask_halfs, .little),
         };
     }
 
-    pub fn matches(self: Instruction, other_pattern: u16) bool {
+    pub fn matches(self: *const InstructionSchema, other_pattern: u16) bool {
         const zero_where_agree = other_pattern ^ self.skeleton;
         const zero_where_agree_and_variable = zero_where_agree & self.mask;
         std.debug.print("zero_where_agree: {d}, zero_where_agree_and_variable: {d}\n", .{ zero_where_agree, zero_where_agree_and_variable });
@@ -124,9 +159,10 @@ const Instruction = struct {
     }
 };
 
+const mov0 = InstructionSchema.parseInstructionSchema("mov0", "100010dw") catch unreachable;
+// const mov1 = Instruction.init("mov1", "1100011w") catch unreachable;
+
 test "check that things match" {
-    const mov0 = Instruction.init("mov0", "100010dw") catch unreachable;
-    // const mov1 = Instruction.init("mov1", "1100011w") catch unreachable;
     try std.testing.expect(mov0.matches(0b10001011 << 8));
     try std.testing.expect(mov0.matches(0b10001000 << 8));
     try std.testing.expect(!mov0.matches(0b10011011 << 8));
