da817fa4c714918a8fdbc34460d0190cf349337b
20muzm01 <20muzm01@gmail.com>
2026-08-29 10:57:27 -0400

    prog in compilation and testing

--- src/disassembler/operands.zig
+++ src/disassembler/operands.zig
@@ -12,7 +12,7 @@ const reg_and_word_to_reg_name = [_][2]*const [2:0]u8{
     .{ "dh", "si" },
     .{ "bh", "di" },
 };
-const rm_to_effective_addr_calc = [_][2]*const [2:0]?u8{
+const rm_to_effective_addr_calc = [_][2]?*const [2:0]u8{
     // if there are two things in the list, the equation these bits code for are those added
     .{ "bx", "si" },
     .{ "bx", "di" },
@@ -29,8 +29,8 @@ const ImmediateOperand = struct {
     value: u16,
     word: bool,
 
-    pub fn fmt(self: *@This(), arr: *std.ArrayList(u8)) void {
-        arr.printBounded("{d}", self.value);
+    pub fn fmt(self: *const @This(), arr: *std.ArrayList(u8)) void {
+        arr.printAssumeCapacity("{d}", .{self.value});
     }
 };
 
@@ -38,8 +38,8 @@ const RegOperand = struct {
     reg_ind: u3,
     word: bool,
 
-    pub fn fmt(self: *@This(), arr: *std.ArrayList(u8)) void {
-        arr.printBounded("{s}", reg_and_word_to_reg_name[self.value]);
+    pub fn fmt(self: *const @This(), arr: *std.ArrayList(u8)) void {
+        arr.printAssumeCapacity("{s}", .{reg_and_word_to_reg_name[self.reg_ind]});
     }
 };
 
@@ -47,8 +47,8 @@ const SegmentRegOperand = struct {
     reg_ind: u3,
     word: bool = true, // true by default
 
-    pub fn fmt(self: *@This(), arr: *std.ArrayList(u8)) void {
-        arr.printBounded("{s}", segment_names[self.value]);
+    pub fn fmt(self: *const @This(), arr: *std.ArrayList(u8)) void {
+        arr.printAssumeCapacity("{s}", .{segment_names[self.reg_ind]});
     }
 };
 
@@ -56,7 +56,7 @@ const RegisterOperand = union(enum) {
     reg_operand: RegOperand,
     seg_operand: SegmentRegOperand,
 
-    pub fn word(self: *@This()) bool {
+    pub fn word(self: *const @This()) bool {
         return switch (self) {
             inline else => self.word,
         };
@@ -68,42 +68,41 @@ const MemoryOperand = struct {
     displacement: u16,
     word: bool,
 
-    // pub fn getSizeSpec(self: *@This()) [4]u8 {
+    // pub fn getSizeSpec( self: *const @This()) [4]u8 {
     //     return switch (self.word) {
     //         true => "word",
     //         false => "byte",
     //     };
     // }
 
-    pub fn fmt(self: *@This(), arr: *std.ArrayList(u8)) void {
-        arr.printBounded("[", .{});
+    pub fn fmt(self: *const @This(), arr: *std.ArrayList(u8)) void {
+        arr.printAssumeCapacity("[", .{});
 
-        const first, const second = if (self.memory_base) |m| {
-            rm_to_effective_addr_calc[m];
-        } else {
+        const first, const second = if (self.memory_base) |m|
+            rm_to_effective_addr_calc[m]
+        else
             .{ null, null };
-        };
 
         var need_plus = false;
         if (first) |f| {
-            arr.printBounded("{s}", .{f});
+            arr.printAssumeCapacity("{s}", .{f});
             need_plus = true;
         }
         if (second) |s| {
             if (need_plus) {
-                arr.printBounded(" + ", .{});
+                arr.printAssumeCapacity(" + ", .{});
             }
-            arr.printBounded("{s}", .{s});
+            arr.printAssumeCapacity("{s}", .{s});
             need_plus = true;
         }
         if (self.displacement != 0) {
             if (need_plus) {
-                arr.printBounded(" + ", .{});
+                arr.printAssumeCapacity(" + ", .{});
             }
-            arr.printBounded("{d}", .{self.displacement});
+            arr.printAssumeCapacity("{d}", .{self.displacement});
         }
 
-        arr.printBounded("]", .{});
+        arr.printAssumeCapacity("]", .{});
     }
 };
 
@@ -112,63 +111,64 @@ const Operand = union(enum) {
     register_operand: RegisterOperand,
     memory_operand: MemoryOperand,
 
-    pub fn fmt(self: *@This(), arr: *std.ArrayList(u8)) void {
+    pub fn fmt(self: *const @This(), arr: *std.ArrayList(u8)) void {
         return switch (self) {
             inline else => |op| op.fmt(arr),
         };
     }
 };
 
-fn test_fmt_helper(expected: *[]u8, actual: *Operand) void {
-    const buf = [_]u8{0} ** 64;
-    const arr = std.ArrayList(u8).initBuffer(buf);
-    actual.fmt(arr);
-    std.testing.expectEqualStrings(expected, arr.items);
+fn test_fmt_helper(expected: []const u8, actual: anytype) !void {
+    var buf = [_]u8{0} ** 64;
+    var arr = std.ArrayList(u8).initBuffer(&buf);
+    actual.fmt(&arr);
+    try std.testing.expectEqualStrings(expected, arr.items);
 }
+
 test "immediate operand" {
-    test_fmt_helper("12", ImmediateOperand{ .value = 12, .word = false });
+    try test_fmt_helper("12", &ImmediateOperand{ .value = 12, .word = false });
 }
 
 test "reg operand byte" {
-    test_fmt_helper("ch", RegOperand{ .reg_ind = 5, .word = false });
+    try test_fmt_helper("ch", &RegOperand{ .reg_ind = 5, .word = false });
 }
 test "reg operand word" {
-    test_fmt_helper("bp", RegOperand{ .reg_ind = 5, .word = true });
+    try test_fmt_helper("bp", &RegOperand{ .reg_ind = 5, .word = true });
 }
 
 test "segment reg operand" {
-    test_fmt_helper("cs", SegmentRegOperand{ .reg_ind = 1, .word = false });
+    try test_fmt_helper("cs", &SegmentRegOperand{ .reg_ind = 1, .word = false });
 }
 
 test "register operand is reg operand" {
-    test_fmt_helper("bp", RegisterOperand{ .reg_operand = .{ .reg_ind = 5, .word = false } });
+    try test_fmt_helper("bp", &RegisterOperand{ .reg_operand = .{ .reg_ind = 5, .word = false } });
 }
 test "register operand is seg operand" {
-    test_fmt_helper("cs", RegisterOperand{ .seg_operand = .{ .reg_ind = 1, .word = false } });
+    try test_fmt_helper("cs", &RegisterOperand{ .seg_operand = .{ .reg_ind = 1, .word = false } });
 }
 
 test "memory operand partial null base only" {
-    test_fmt_helper("[di]", MemoryOperand{ .memory_base = 5, .displacement = 0, .word = false });
+    try test_fmt_helper("[di]", &MemoryOperand{ .memory_base = 5, .displacement = 0, .word = false });
 }
 test "memory operand no null base only" {
-    test_fmt_helper("[bx + si]", MemoryOperand{ .memory_base = 5, .displacement = 0, .word = false });
+    try test_fmt_helper("[bx + si]", &MemoryOperand{ .memory_base = 5, .displacement = 0, .word = false });
 }
 test "memory operand displacement only" {
-    test_fmt_helper("[4]", MemoryOperand{ .memory_base = null, .displacement = 4, .word = false });
+    try test_fmt_helper("[4]", &MemoryOperand{ .memory_base = null, .displacement = 4, .word = false });
 }
 test "memory operand partial null base and displacement" {
-    test_fmt_helper("[di + 4]", MemoryOperand{ .memory_base = 5, .displacement = 4, .word = false });
+    try test_fmt_helper("[di + 4]", &MemoryOperand{ .memory_base = 5, .displacement = 4, .word = false });
 }
 test "memory operand no null base and displacement" {
-    test_fmt_helper("[bx + si + 4]", MemoryOperand{ .memory_base = 5, .displacement = 4, .word = false });
+    try test_fmt_helper("[bx + si + 4]", &MemoryOperand{ .memory_base = 5, .displacement = 4, .word = false });
 }
 
 test "operand immediate" {
-    test_fmt_helper("12", Operand{ .immediate_operand = .{ .value = 12, .word = false } });
+    try test_fmt_helper("12", &Operand{ .immediate_operand = .{ .value = 12, .word = false } });
 }
 test "operand register" {
-    test_fmt_helper("bp", Operand{ .register_operand = .{ .reg_operand = .{ .reg_ind = 5, .word = false } } });
+    try test_fmt_helper("bp", &Operand{ .register_operand = .{ .reg_operand = .{ .reg_ind = 5, .word = false } } });
 }
 test "operand memory" {
-    test_fmt_helper("[bx + si + 4]", Operand{ .memory_operand = .{ .memory_base = 5, .displacement = 4, .word = false } });
+    try test_fmt_helper("[bx + si + 4]", &Operand{ .memory_operand = .{ .memory_base = 5, .displacement = 4, .word = false } });
 }
